b55d6839b5e1db71da79cb4e7242322809e7f1c3
riqvade <40538535+riqvade@users.noreply.github.com>
2022-06-25 15:08:40 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿
+Dictionary<int, int> descedants = new Dictionary<int, int>();
+
+descedants.Add(1, 0);
+descedants.Add(2, 0);
+descedants.Add(3, 2);
+descedants.Add(4, 2);
+descedants.Add(5, 1);
+descedants.Add(6, 4);
+descedants.Add(7, 0);
+descedants.Add(8, 4);
+descedants.Add(9, 3);
+descedants.Add(10, 3);
+descedants.Add(11, 11);
+
+
+bool result = isChildOf(11, 11);
+
+Console.WriteLine(result);
+
+
+/// <summary>
+/// Is nodes are descedants
+/// </summary>
+bool isChildOf(int node1, int node2)
+{
+    foreach (KeyValuePair<int, int> descedant in descedants)
+    {
+        if (descedant.Key == node1 && descedant.Value == node2)
+        {
+            return true;
+        }
+    }
+
+    int key = descedants[node1];
+
+    if (key == 0)
+    {
+        return false;
+    }
+
+    return isChildOf(key, node2); ;
+}
---
+++ Relatives/5.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionMethod.Relatives
+{
+    internal class _5 : _1
+    {
+
+    }
+}
